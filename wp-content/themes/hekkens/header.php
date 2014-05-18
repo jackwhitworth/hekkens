d5c809e5b5843729997e324c619fe0db4e1d3622
Nicolay Hekkens <nicolay@wednesdayagency.com>
2014-05-18 15:37:31 +0100

    Header update. Nav bar with args.

--- wp-content/themes/hekkens/header.php
+++ wp-content/themes/hekkens/header.php
@@ -29,23 +29,32 @@
 		
 		<header>
 			
-			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				
+			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">	
 				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-			
 			</a>
 		
-			<nav role="navigation">
-				
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'main-navigation' ) ); ?>
-				
-			</nav>
+			<?php
+				$defaults = array(
+					'theme_location'  => '',
+					'menu'            => '',
+					'container'       => 'nav',
+					'container_class' => '',
+					'container_id'    => '',
+					'menu_class'      => 'menu',
+					'menu_id'         => '',
+					'echo'            => true,
+					'fallback_cb'     => 'wp_page_menu',
+					'before'          => '',
+					'after'           => '',
+					'link_before'     => '',
+					'link_after'      => '',
+					'items_wrap'      => '<ul>%3$s</ul>',
+					'depth'           => 0,
+					'walker'          => ''
+				);
 
-			<div class="search">
-				
-				<?php get_search_form(); ?>
-
-			</div>	
+				wp_nav_menu( $defaults );
+			?>
 		
 		</header><!-- #masthead -->
 
